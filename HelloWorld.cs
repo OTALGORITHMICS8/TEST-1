706bebcca05a4c931f52560457f6969b24797f80
OTALGORITHMICS8 <otmoreno2013@gmail.com>
2024-12-20 20:12:13 +0100

    AFEGIR UN NOU ARXIU

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //s'executa 1 cop al principi  
+        print("hola");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //S'executa mes d'un cop
+        print("hola");
+    }
+}
